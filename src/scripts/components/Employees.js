ad8275325773e8c7a8216c36e12137f5ffebcb6a
Nixon Lolila Ramin <nixonlr@gmail.com>
2015-03-08 00:40:17 -0500

    about to merge with master

--- src/scripts/components/Employees.js
+++ src/scripts/components/Employees.js
@@ -1,14 +1,17 @@
 'use strict';
 
 var React = require('react/addons');
+var Employee = require('./Employee');
 
 require('styles/Employees.css');
 
 var Employees = React.createClass({
   render: function () {
+		console.log(this.props.employeeClickHandler);
     return (
       <div id='Employees'>
-      	{this.prop.choices.map(function (employee, i) {
+
+      	{this.props.choices.map(function (employee, i) {
       			return(
       				<div className='Employee' key={i}>
 	      				<Employee name={employee.name} url={employee.url} onClick={this.props.employeeClickHandler} />
